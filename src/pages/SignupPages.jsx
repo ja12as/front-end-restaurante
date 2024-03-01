b687898ce9e7b9f1a19da6a4441081c68a8df0d5
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2024-03-01 01:48:16 -0500

    RESTAURANTE-V2-CREACION_REGISTRO_USUARIO

--- src/pages/SignupPages.jsx
+++ src/pages/SignupPages.jsx
@@ -1,11 +1,29 @@
-
+import {useForm} from 'react-hook-form'
 
 function SignupPages() {
+    const {register, handleSubmit, errors} = useForm();
     return (
         <div>
-        hola este es el lregistro
+            <h1>Registro de usuario</h1>
+            <form>
+                <div>
+                    <input type="text" {...register( "name",{required: true}) } placeholder='name'/>
+                    <input type="text" {...register( "name",{required: true}) } placeholder='name'/>
+                    <input type="email" {...register( "name",{required: true}) } placeholder='name'/>
+                    <input type="text"{...register( "name",{required: true}) } placeholder='name'/>
+                </div>
+                <div>
+                    <input type="text" {...register( "name",{required: true}) } placeholder='name'/>
+                    <input type="password" {...register( "name",{required: true}) } placeholder='name'/>
+                    <input type="text" {...register( "name",{required: true}) } placeholder='name'/>
+                    <input type="text"{...register( "name",{required: true}) } placeholder='name'/>
+                </div>
+                <button type='submit'>Registrar</button>
+                <button type='submit'>Salir</button>
+            </form>
         </div>
     )
+    
 }
 
 export default SignupPages
