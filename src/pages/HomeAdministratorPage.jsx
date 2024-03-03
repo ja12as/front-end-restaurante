2d167ca4e0b5605a1a38323f3f163f591864bb84
Jose Armando Avendaño Saucedo <126033004+ja12as@users.noreply.github.com>
2024-03-03 16:50:26 -0500

    RESTAURANTE-V2-PARTE-DE-YORDAN

--- src/pages/HomeAdministratorPage.jsx
+++ src/pages/HomeAdministratorPage.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import '../style/SignupStyle.css';
 
 function HomeAdministratorPage() {
     return (
-        <div>
-        
+        <div className="container">
+        <div className="header">
+            <div className="perfil-empleado">
+            <img src="../img/usuario.jpg" alt="img-empleado" />
+            <p>hola-nom-empleado</p>
+            </div>
         </div>
-    )
+
+        <div className="main-content-adm">
+            <div className="img-medio-admin">
+            <img src="../img/registar compras.png" alt="Registro-venta" />
+            <p>Registrar venta</p>
+            </div>
+
+            <div className="img-medio-admin">
+            <img src="../img/historial factura.png" alt="Historial-factura" />
+            <p>Historial factura</p>
+            </div>
+
+            <div className="img-medio-admin1">
+            <img src="../img/menu 1.png" alt="Registro-menu" />
+            <p>Registro menu</p>
+            </div>
+
+            <div className="img-medio-admin1">
+            <img src="../img/perfil 1.png" alt="perfil" />
+            <p>Perfiles</p>
+            </div>
+        </div>
+
+        <div className="main-content-adm">
+            <div className="img-medio-admin">
+            <img src="../img/registar compras.png" alt="Registro-compra" />
+            <p>Registro compras</p>
+            </div>  
+
+            <div className="img-medio-admin">
+            <img src="../img/recibo pago.png" alt="Historial-factura" />
+            <p>Recibo pago</p>
+            </div>
+
+            <div className="img-medio-admin">
+            <img src="../img/proveedor.png" alt="Registro-venta" />
+            <p>Proveedor</p>
+            </div>
+        </div>
+
+        <div className="action-button">
+            <button className="action-btn">Cerrar Sesión</button>
+        </div>
+        </div>
+    );
 }
 
-export default HomeAdministratorPage
+export default HomeAdministratorPage;
--- src/pages/HomeCashierPage.jsx
+++ src/pages/HomeCashierPage.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import '../style/SignupStyle.css';
 
 function HomeCashierPage() {
     return (
-        <div>
-        
+        <div className="container">
+        <div className="header">
+            <div className="perfil-empleado">
+            <img src="../img/usuario.jpg" alt="img-empleado" />
+            <p>hola-nom-empleado</p>
+            </div>
         </div>
-    )
+
+        <div className="main-content">
+            <Link to="/ruta-imagen-1" style={{ textDecoration: "none" }}>
+            <div className="img-medio-cajero">
+                <img
+                src="../img/registar compras.png"
+                alt="Registro-venta"
+                style={{
+                    maxWidth: "50%",
+                    margin: "2%",
+                    paddingRight: "50px",
+                    paddingLeft: "40px",
+                }}
+                />
+                <p
+                style={{
+                    color: "black",
+                    textDecoration: "overLine",
+                    textDecorationColor: "#5AD57F",
+                    textDecorationThickness: "3px",
+                }}
+                >
+                Registrar ventas
+                </p>
+            </div>
+            </Link>
+
+            <Link to="/HomeAdministratorPage" style={{ textDecoration: "none" }}>
+            <div className="/src/pages/HomeAdministratorPage.jsx">
+                <img
+                src="../img/historial factura.png"
+                alt="Historial-factura"
+                style={{
+                    maxWidth: "50%",
+                    margin: "2%",
+                    paddingRight: "50px",
+                    paddingLeft: "40px",
+                }}
+                />
+                <p
+                style={{
+                    color: "black",
+                    textDecoration: "overLine",
+                    textDecorationColor: "#5AD57F",
+                    textDecorationThickness: "3px",
+                }}
+                >
+                Historial de facturas
+                </p>
+            </div>
+            </Link>
+        </div>
+
+        <div className="action-button">
+            <button className="action-btn">Cerrar Sesión</button>
+        </div>
+        </div>
+    );
 }
 
-export default HomeCashierPage
+export default HomeCashierPage;
